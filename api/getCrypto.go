3ff0a7f9cddf1915c62731c54477ba1607415368
Даниил <dazevich@users.noreply.github.com>
2020-07-09 22:46:58 +0200

    2
    2

--- api/getCrypto.go
+++ api/getCrypto.go
@@ -2,37 +2,100 @@ package api
 
 import (
 	"ServerSide/api/bhx"
-	"fmt"
+	"encoding/hex"
+	"encoding/json"
 	"log"
 	"net/http"
 )
 
-type CryptoAnser struct {
+type CryptoAnswer struct {
+	Key            string
+	Input          string
+	Output         string
+	DecryptedInput string
 }
 
-func GetCrypto(wr http.ResponseWriter, r *http.Request) {
+func Encrypt(input string, key bhx.BoxSharedKey, nonce bhx.BoxNonce) []byte {
 
-	// Test crypto
-	key := r.FormValue("key")
-	text := []byte(r.FormValue("text"))
+	result, err := bhx.Encrypt([]byte(input), key, nonce)
+	if err != nil {
+		log.Fatal(err.Error())
+		return nil
+	}
 
-	var keyBx bhx.BoxSharedKey
-	copy(keyBx[:], bhx.Sha256H([]byte(key)).Bytes())
+	return result
+}
 
-	nnonce := bhx.GetKeyNonce(keyBx)
+func Decrypt(input []byte, key bhx.BoxSharedKey, nonce bhx.BoxNonce) []byte {
 
-	result, err := bhx.Encrypt(text, keyBx, nnonce)
+	result, err := bhx.Decrypt(input, key, nonce)
+	if err != nil {
+		log.Fatal(err.Error())
+		return nil
+	}
+
+	return result
+}
+
+func GetCrypto(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	k := r.FormValue("key")
+	in := r.FormValue("input")
+	out := r.FormValue("output")
+
+	var input []byte
+	var output []byte
+	var key bhx.BoxSharedKey
+
+	if k != "" {
+		bytes, err := hex.DecodeString(k)
+		if err != nil {
+			log.Fatal("Error create key: ", err)
+		}
+		copy(key[:], bytes)
+	} else {
+		bytes, err := hex.DecodeString("edeef0")
+		if err != nil {
+			log.Fatal("Error create key: ", err)
+		}
+		copy(key[:], bytes)
+	}
+
+	var nnonce bhx.BoxNonce
+	kh, err := hex.DecodeString("edeef0edeef0edeef0edeef0edeef0edeef0edeef0edeef0")
 	if err != nil {
 		log.Fatal(err)
 	}
+	copy(nnonce[:], kh[:bhx.BoxNonceLen])
 
-	fmt.Println(string(result))
-
-	output, err := bhx.Decrypt(result, keyBx, nnonce)
-	if err != nil {
-		log.Fatalln(err)
+	if out == "" && in != "" {
+		output = Encrypt(in, key, nnonce)
+		input = []byte(in)
 	}
 
-	fmt.Println(string(output))
+	if out != "" && in == "" {
+		input = Decrypt([]byte(out), key, nnonce)
+		output = []byte(out)
+	}
 
+	answer := &CryptoAnswer{}
+
+	keyString := string(key[:len(key)])
+	outputString := string(output[:len(output)])
+	decInputString := string(input[:len(input)])
+
+	answer.Key = keyString
+	answer.Input = string(in)
+	answer.Output = outputString
+	answer.DecryptedInput = decInputString
+
+	jsonAnsw, err := json.Marshal(answer)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	w.Write(jsonAnsw)
 }
