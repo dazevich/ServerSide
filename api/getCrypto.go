2d1444aecfe3bdbcb9f78f3cb5bd023d0dafdfb1
Даниил <dazevich@users.noreply.github.com>
2020-07-10 20:46:18 +0200

    5
    5

--- api/getCrypto.go
+++ api/getCrypto.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 )
 
+// Структура для преобразования в JSON
 type CryptoAnswer struct {
 	Key    string
 	Input  string
@@ -20,6 +21,7 @@ var (
 	output string
 )
 
+// GetKey... Пользовательский ключ складывается с шаблоном так, чтобы итоговый ключ состоял из 64 символов
 func GetKey(key string) ([]byte, error) {
 	userKey := []byte(key)
 	keyTmp := []byte("6368616e176520746869732070617373776f726420746f206120736563726574")
@@ -43,11 +45,13 @@ func GetKey(key string) ([]byte, error) {
 	return result, nil
 }
 
+// GetNonce... Получение номера
 func GetNonce() []byte {
 	result, _ := hex.DecodeString("64a9433eae7ccceee2fc0eda")
 	return result
 }
 
+// Encrypt... Шифрование текста с помощью полученного ключа и номера
 func Encrypt(text string, key []byte, nonce []byte) []byte {
 	textByte := []byte(text)
 	block, err := aes.NewCipher(key)
@@ -62,6 +66,7 @@ func Encrypt(text string, key []byte, nonce []byte) []byte {
 	return ciphertext
 }
 
+// Decrypt... Дешифровка зашифрованного текста с помощью ключа и номера
 func Decrypt(ciphertext []byte, key []byte, nonce []byte) []byte {
 	block, err := aes.NewCipher(key)
 	if err != nil {
@@ -80,9 +85,11 @@ func Decrypt(ciphertext []byte, key []byte, nonce []byte) []byte {
 
 func GetCrypto(w http.ResponseWriter, r *http.Request) {
 
+	// Разрешаю подключение с различных доменов
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
+	// Получаю ключ, текст и шифротекст
 	userKey := r.FormValue("key")
 	in := r.FormValue("input")
 	out := r.FormValue("output")
@@ -91,12 +98,16 @@ func GetCrypto(w http.ResponseWriter, r *http.Request) {
 		userKey = "edeef0"
 	}
 
+	// Привожу ключ к нужному виду
 	key, err := GetKey(userKey)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
+
+	// Получаю номер
 	nonce := GetNonce()
 
+	// Если дан только текст, шифрую его
 	if in == "" && out != "" {
 		decodedStr, err := hex.DecodeString(out)
 		if err != nil {
@@ -106,11 +117,13 @@ func GetCrypto(w http.ResponseWriter, r *http.Request) {
 		output = out
 	}
 
+	// Если дан только шифротекст, дешифрую его
 	if in != "" && out == "" {
 		output = hex.EncodeToString(Encrypt(in, key, nonce))
 		input = in
 	}
 
+	// Возвращаю ответ в формате JSON
 	answer := &CryptoAnswer{}
 	answer.Key = userKey
 	answer.Input = input
